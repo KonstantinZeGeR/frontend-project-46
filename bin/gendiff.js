14ec02b20174f549e81ce20e7fda04df4be0901a
KonstantinZeGeR <zegelr23@gmail.com>
2024-09-10 20:45:03 +0400

    Updated Environment and add src/compareFiles

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-
 import { program } from "commander";
+import compareFiles from "../src/compareFiles.js";
 
 program
   .version("0.0.1")
   .description("Compares two configuration files and shows a difference.")
-  .option("-c, --config <type>", "set config");
-program.helpOption("-h, --help", "display help for command");
-program.parse(process.argv);
+  .option("-f, --format [type]", "output format")
+  .arguments("<filepath1> <filepath2>")
+  .action((filepath1, filepath2) => {
+    compareFiles(filepath1, filepath2);
+  })
+  .parse(process.argv);
---
+++ src/compareFiles.js
@@ -0,0 +1,5 @@
+export default (filepath1, filepath2) => {
+  // Здесь будет логика сравнения файлов
+  console.log(`File 1: ${filepath1}`);
+  console.log(`File 2: ${filepath2}`);
+};
