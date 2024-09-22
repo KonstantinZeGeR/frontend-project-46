fa5d7377a0089a374514f50a70857901e9c0605c
KonstantinZeGeR <zegelr23@gmail.com>
2024-09-22 17:35:05 +0400

    update gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import parseFile from '../src/parser.js';
 import genDiff from '../src/index.js';
 
 const program = new Command();
@@ -11,11 +10,7 @@ program
   .option('-f, --format [type]', 'output format')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
-    const data1 = parseFile(filepath1); // Добавлено для парсинга данных из файлов
-    const data2 = parseFile(filepath2);
-    const diff = genDiff(data1, data2);
-    console.log('Data from file1:', JSON.stringify(data1, null, 2));
-    console.log('Data from file2:', JSON.stringify(data2, null, 2));
+    const diff = genDiff(filepath1, filepath2);
     console.log(diff);
 
     // Здесь далее можно будет добавить логику сравнения данных
